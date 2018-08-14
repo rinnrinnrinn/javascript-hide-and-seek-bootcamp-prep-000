216939a3a8e8652d4a9e6523ba627eba7ba85be1
Caitlin Lauer <caitlinrinn@gmail.com>
2018-08-14 09:17:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  var selectorName = document.querySelector(selector);
+  return selectorName;
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target')
+}
+
+function increaseRankBy(n) {
+  const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list')
+   for (let i = 0; i < upRank.length; i++){
+    upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + n)
+}}
+
+function deepestChild() {
+  var child = document.getElementById('grand-node').querySelectorAll('div');
+  return child[child.length - 1];
+}
