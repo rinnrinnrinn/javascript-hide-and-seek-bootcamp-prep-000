cf934f48104009bec12c50ea68e8f45bca8ee1e2
Caitlin Lauer <caitlinrinn@gmail.com>
2018-08-14 08:59:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ function getFirstSelector(selector) {
   return selectorName;
 }
 
-function nestedTarget(element, criteriaFn) {
-  for (let i = 0; i < element.length; i++) {
-    if (criteriaFn(element[i])) {
-      return element[i]
-    }
-  }
+function nestedTarget() {
+  return document.getElementByID
 }
